33ae92f2d4dff824583561ef44a65670f36bb593
yamineez <yaminee.chaudhary.btech2022@sitpune.edu.in>
2024-11-14 03:16:24 +0530

    Update 5zok.cpp

--- 5zok.cpp
+++ 5zok.cpp
@@ -6,26 +6,21 @@ struct Item {
     int weight;
 };
 
-//0/1 Knapsack
 int knapsack(int n, int W, Item items[]) {
-    int dp[n + 1][W + 1];  // Create a 2D array to store results
+    int dp[n + 1][W + 1];
 
-    //table
     for (int i = 0; i <= n; i++) {
         for (int w = 0; w <= W; w++) {
             if (i == 0 || w == 0) {
-                dp[i][w] = 0;  //base case: 0 items or 0 weight capacity
+                dp[i][w] = 0;
             } else if (items[i - 1].weight <= w) {
-                //max of current item wt+ / prev item ?
                 dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - items[i - 1].weight] + items[i - 1].profit);
             } else {
-                // If the current item's weight is more than the current weight capacity, don't take it
                 dp[i][w] = dp[i - 1][w];
             }
         }
     }
 
-    // The result is stored in dp[n][W], i.e., the maximum profit
     return dp[n][W];
 }
 
@@ -37,13 +32,12 @@ int main() {
     cout << "Enter the maximum weight capacity of the knapsack: ";
     cin >> W;
 
-    Item items[n];  // Array of items
+    Item items[n];
     cout << "Enter the profit and weight of each item:\n";
     for (int i = 0; i < n; i++) {
         cin >> items[i].profit >> items[i].weight;
     }
 
-    // Call the knapsack function and print the result
     int maxProfit = knapsack(n, W, items);
     cout << "Maximum profit that can be obtained: " << maxProfit << endl;
 
